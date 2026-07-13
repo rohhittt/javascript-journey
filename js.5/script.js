423a9578a8499afcfd720f0293e7b8cee09017c1
Rohit Munday <rohitmunday31@gmail.com>
2026-07-13 13:10:32 +0530

    Learn JavaScript arrays and indexing

---
+++ js.5/script.js
@@ -0,0 +1,17 @@
+const games = [
+    "Valorant",
+    "League of Legends",
+    "Counter Strike",
+    "Battle Grounds",
+    "Minefcraft",
+    "Last Game"
+]
+
+console.log(games[1]);
+console.log(games[3]);
+console.log(games[5]);
+
+
+
+
+
