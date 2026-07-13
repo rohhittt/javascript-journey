2e5b827f7b3621ba4fe101af92e69c1f6f7c8031
Rohit Munday <rohitmunday31@gmail.com>
2026-07-13 17:28:45 +0530

    Learn loops, arrays, and build movie manager

---
+++ js.7/script.js
@@ -0,0 +1,7 @@
+const movielist = document.getElementById('movielist')
+
+button.addEventListener("click",function()){
+    if (id === "addmovie") {
+        movielist.textcontent = "Movie Added";
+    }
+}
